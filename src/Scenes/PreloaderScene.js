0e2cae39524921bf4ebca7ff322da5c94db0fc28
ventrosky <ventrosky@develer.com>
2020-09-07 23:30:22 +0200

    phaser 3 scenes template

---
+++ src/Scenes/PreloaderScene.js
@@ -0,0 +1,104 @@
+import Phaser from 'phaser';
+import imgLogo from '../assets/logo.png';
+import buttonImg01 from '../assets/ui/blue_button00.png';
+import buttonImg02 from '../assets/ui/blue_button01.png';
+import boxImg from '../assets/ui/grey_box.png';
+import checkedBoxImg from '../assets/ui/blue_boxCheckmark.png';
+import bgMusicImg from '../assets/ElectronicFantasy.ogg';
+
+export default class PreloaderScene extends Phaser.Scene {
+  constructor() {
+    super('Preloader');
+  }
+
+  init() {
+    this.readyCount = 0;
+  }
+
+  preload() {
+    // add logo image
+    this.add.image(400, 200, 'logo');
+
+    // display progress bar
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    // update progress bar
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    // update file progress text
+    this.load.on('fileprogress', (file) => {
+      assetText.setText(`Loading asset: ${file.key}`);
+    });
+
+    // remove progress bar when complete
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+      this.ready();
+    });
+
+    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+
+    // load assets needed in our game
+    this.load.image('blueButton1', buttonImg01);
+    this.load.image('blueButton2', buttonImg02);
+    this.load.image('logo', imgLogo);
+    this.load.image('box', boxImg);
+    this.load.image('checkedBox', checkedBoxImg);
+    this.load.audio('bgMusic', [bgMusicImg]);
+  }
+
+  ready() {
+    this.scene.start('Title');
+    this.readyCount += 1;
+    if (this.readyCount === 2) {
+      this.scene.start('Title');
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import config from './Config/config';
+import GameScene from './Scenes/GameScene';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
+import OptionsScene from './Scenes/OptionsScene';
+import CreditsScene from './Scenes/CreditsScene';
 
-function preload() {
-  this.load.image('logo', logoImg);
+import Model from './Model';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    const model = new Model();
+    this.globals = { model, bgMusic: null };
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Boot');
+  }
 }
 
-function create() {
-  const logo = this.add.image(400, 150, 'logo');
-
-  this.tweens.add({
-    targets: logo,
-    y: 400,
-    duration: 2000,
-    ease: 'Power2',
-    yoyo: true,
-    loop: -1,
-  });
-}
-
-const config = {
-  type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
-  scene: {
-    preload,
-    create,
-  },
-};
-
-// eslint-disable-next-line no-unused-vars
-const game = new Phaser.Game(config);
+window.game = new Game();
